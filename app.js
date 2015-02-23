fe44546503eef68c95fe7644007a553c6bf7dcf9
Aesop Wolfe <aesopwolf@gmail.com>
2015-02-22 22:28:56 -0800

    api endpoints

--- app.js
+++ app.js
@@ -4,77 +4,123 @@ var config = require('./config'),
     lame = require('lame'),
     Speaker = require('speaker'),
     fs = require('fs'),
-    http = require('http');
+    http = require('http'),
+    express = require('express');
 
-console.log(config);
-
+var app = express();
 var audioOptions = {channels: 2, bitDepth: 16, sampleRate: 44100};
+var nextSong, myCountry;
 
-client.authenticate(config.userEmail, config.userPassword, function(err, body) {
 
-  client.request('getArtistSearchResults', {query: process.argv[2], limit: 1}, function(err, status, body) {
+app.get('/api/stop', function (req, res) {
+  // app.locals.jukebox.stop();
+  res.send('stop the music');
+});
+
+app.get('/api/status', function (req, res) {
+  res.send(nextSong);
+});
+
+app.get('/api/play/:artist', function (request, response) {
+  client.request('getArtistSearchResults', {query: request.params.artist, limit: 1}, function(err, status, body) {
+    console.log(err, status, body);
     var artist = body.artists[0].ArtistID;
 
-    client.request('getCountry', {}, function(err, status, body) {
-      var myCountry = body;
+    playMusic(undefined, artist, function(data) {
+      console.log(data);
+      response.send(data);
+    });
 
-      function playMusic(autoplayState) {
-        var decoder = lame.Decoder();
-        var speaker = new Speaker(audioOptions);
+  });
+});
 
-        client.request('startAutoplay', {"artistIDs":[artist], "songIDs":[], "autoplayState": autoplayState}, function(err, status, body) {
-          var parameters = {
-            songID: body.nextSong.SongID,
-            country: myCountry,
-            lowBitrate: false
+
+// var Jukebox = (function (autoplayState, artist, cb) {
+//   var instance;
+//
+//   function createInstance() {
+//     return {
+//       decoder: lame.Decoder(),
+//       speaker: new Speaker(audioOptions)
+//     }
+//   };
+//
+//   return {
+//     getInstance: function() {
+//       if(!instance) {
+//         instance = createInstance();
+//       }
+//       return instance;
+//     },
+//     stop: function() {
+//       instance.decoder = undefined;
+//       instance.speaker = undefined;
+//     }
+//   };
+// })();
+
+function playMusic(autoplayState, artist, cb) {
+  var decoder = lame.Decoder();
+  var speaker = new Speaker(audioOptions);
+
+  client.request('startAutoplay', {"artistIDs":[artist], "songIDs":[], "autoplayState": autoplayState}, function(err, status, body) {
+    var parameters = {
+      songID: body.nextSong.SongID,
+      country: myCountry,
+      lowBitrate: false
+    };
+
+    autoplayState = body.autoplayState;
+    nextSong = body.nextSong;
+
+    client.request('getStreamKeyStreamServer', parameters, function(err, status, body) {
+      var getStreamKeyStreamServer = body;
+      http.get(body.url, function(res) {
+        var playedFor30;
+        setTimeout(function() {
+          var payload = {
+            streamKey: getStreamKeyStreamServer.StreamKey,
+            streamServerID: getStreamKeyStreamServer.StreamServerID,
+            sessionID: client.sessionID
           };
 
-          console.log(body);
-          autoplayState = body.autoplayState;
-          var nextSong = body.nextSong;
+          client.request('markStreamKeyOver30Secs', payload, function(err, status, body) {
+            playedFor30 = true;
+          });
 
-          client.request('getStreamKeyStreamServer', parameters, function(err, status, body) {
-            var getStreamKeyStreamServer = body;
-            http.get(body.url, function(res) {
-              var playedFor30;
-              setTimeout(function() {
-                var payload = {
-                  streamKey: getStreamKeyStreamServer.StreamKey,
-                  streamServerID: getStreamKeyStreamServer.StreamServerID,
-                  sessionID: client.sessionID
-                };
+        }, 30000);
 
-                client.request('markStreamKeyOver30Secs', payload, function(err, status, body) {
-                  console.log("markStreamKeyOver30Secs: ", body);
-                  playedFor30 = true;
-                });
+        if(cb) cb(nextSong);
 
-              }, 30000);
+        res.pipe(decoder).pipe(speaker);
+        speaker.on('flush', function(){
+          console.log("song is done");
+          var payload = {
+            sessionID: client.sessionID,
+            songID: nextSong.SongID,
+            streamKey: getStreamKeyStreamServer.StreamKey,
+            streamServerID: getStreamKeyStreamServer.StreamServerID
+          };
 
-              res.pipe(decoder).pipe(speaker);
-              speaker.on('flush', function(){
-                console.log("song is done");
-                var payload = {
-                  sessionID: client.sessionID,
-                  songID: nextSong.SongID,
-                  streamKey: getStreamKeyStreamServer.StreamKey,
-                  streamServerID: getStreamKeyStreamServer.StreamServerID
-                };
-
-                if(playedFor30) {
-                  client.request('markSongComplete', payload, function(err, status, body) {
-                    console.log(err);
-                    console.log(status);
-                    console.log(body);
-                    playMusic(autoplayState);
-                  });
-                }
-              });
-            })
-          })
+          if(playedFor30) {
+            client.request('markSongComplete', payload, function(err, status, body) {
+              playMusic(autoplayState, artist);
+            });
+          }
         });
-      };
-      playMusic();
-    });
+      })
+    })
+  });
+};
+
+var server = app.listen(3000, function() {
+  console.log("listening");
+  client.authenticate(config.userEmail, config.userPassword, function(err, body) {
+    if(!err) {
+      client.request('getCountry', {}, function(err, status, body) {
+        myCountry = body;
+        // app.locals.jukebox = Jukebox.getInstance();
+      })
+    }
   });
 });
